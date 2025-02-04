4f1e32d18091dac034018c21ddd71cc1572c598f
hhyufan <1838248655@qq.com>
2025-02-04 16:52:23 +0800

    fix(test_r3)：尝试修复中间件问题

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ export const config = { matcher: '/((?!api|static|.*\\..*|_next).*)' };
 
 export function middleware(req) {
     const url = new URL(req.url);
-    const ua = req.headers.get('user-agent') || '';
+    const ua = req.headers['user-agent'] || '';
     const isMobile = /mobile|android|iphone|ipad|phone/i.test(ua.toLowerCase());
     console.log("vercel警告middleware中间件检测")
     // 跳过已处理的移动子域名
