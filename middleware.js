f3694d373e4696187bb0d7b94b812a61cc293a44
hhyufan <1838248655@qq.com>
2025-02-04 17:30:20 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,10 +2,10 @@ export const config = { matcher: '/((?!api|static|.*\\..*|_next).*)' };
 
 export function middleware(req) {
     const url = new URL(req.url);
-    const ua = req.headers['user-agent'] || '';
+    const ua = req.headers['user-agent'] || 'android';
     const isMobile = /mobile|android|iphone|ipad|phone/i.test(ua.toLowerCase());
     // 跳过已处理的移动子域名
-    if (url.hostname === 'miaogu.top' && isMobile) {
+    if (url.hostname.includes('miaogu.top') && isMobile) {
         return Response.redirect(`https://genshin.titlecan.cn/`, 302);
     }
 }
