7859f302d97755012c8f509f1f7acdf891030c13
hhyufan <1838248655@qq.com>
2025-02-04 17:06:12 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,10 +1,9 @@
-export const config = { matcher: '/((?!api|.*\\..*|_next).*)' };
+export const config = { matcher: '/((?!api|static|.*\\..*|_next).*)' };
 
 export function middleware(req) {
     const url = new URL(req.url);
     const ua = req.headers['user-agent'] || '';
     const isMobile = /mobile|android|iphone|ipad|phone/i.test(ua.toLowerCase());
-    console.log("vercel警告middleware中间件检测")
     // 跳过已处理的移动子域名
     if (url.hostname === 'miaogu.top' && isMobile) {
         return Response.redirect(`https://genshin.titlecan.cn/`, 302);
