76ee1d493b56e353a22b5f8d98ced18b15c9b704
hhyufan <1838248655@qq.com>
2025-02-04 17:57:27 +0800

    feat(vercel):启动前检查用户的用户代理（User Agent），如果为移动端，跳转到原神测试网页

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import router from './router/index';
 // import './mock';
 import store from "@/vuex/store.js"; // 引入 Mock 数据
 const app = createApp(App);
+// 检查用户代理并重定向
+router.beforeEach((to, from, next) => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobile = /android|iphone|ipad|ipod|iemobile|opera mini|mobile/i.test(userAgent);
+
+    // 如果是移动设备并且访问的不是移动端页面，重定向到移动端页面
+    if (isMobile && to.path !== '/mobile') {
+        window.location.href = 'https://genshin.titlecan.cn/';
+    } else {
+        next(); // 继续正常路由
+    }
+});
 app.use(store);
 app.use(router);
 app.use(ElementPlus);
