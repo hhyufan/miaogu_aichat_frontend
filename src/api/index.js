1edeaf9bf00e0419e7e20ed995379b59f6eec0dd
hhyufan <1838248655@qq.com>
2025-02-15 14:18:30 +0800

    feat: 修复异步问题，解决期约后才创建base对象

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,30 @@
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 let baseUrl = null;
 let isBaseUrlReady = false;
-let baseUrlPromise = new Promise(_resolve => {})
+let baseUrlPromise = Promise.resolve(); // 默认是已解决的 Promise
+
 if (!isLocalhost) {
     baseUrlPromise = fetch('/api/config')
         .then(response => response.json())
         .then(({ apiUrl }) => {
+            console.log("apiUrl", apiUrl);
             baseUrl = apiUrl;
             isBaseUrlReady = true;
         })
         .catch(console.error);
 }
-(async () =>  {
-    if (isBaseUrlReady && !isLocalhost) {
-        await baseUrlPromise;
-    }
-})()
-console.log("current baseUrl: ", baseUrl)
-const base = {
-    baseUrl
-};
 
+// 使用 async IIFE 确保在 baseUrl 被赋值后再创建 base 对象
+const base = (async () => {
+    await baseUrlPromise; // 等待 baseUrlPromise 完成
+    return {
+        baseUrl: baseUrl ?? 'https://5bb6-59-44-118-74.ngrok-free.app'
+    };
+})();
+
+base.then(result => {
+    console.log(result.baseUrl); // 在这里可以安全地使用 baseUrl
+});
+
+// 导出 base 对象
 export default base;
