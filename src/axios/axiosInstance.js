d9a4911f30d6026ed76995a727b6ea35a1acc474
hhyufan <1838248655@qq.com>
2025-02-15 16:14:37 +0800

    fix(axios): 修复vercel不支持全局await，以及补回TokenUpdated后的处理

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -6,12 +6,13 @@ import {refreshToken, updateToken} from '@/api/getData';
 import {toast} from "@/plugins/toast.js";
 
 // 创建 Axios 实例
-const axiosInstance = axios.create({
-    baseURL: await getBaseUrl(), // 提前解析 baseUrl
-});
+const axiosInstance = axios.create();
 
 // 请求拦截器
 axiosInstance.interceptors.request.use(async (config) => {
+    if(!config.baseURL) {
+        config.baseURL = await getBaseUrl();
+    }
     const ignoredUrls = ['/user/login', '/user/register', '/user/refresh'].map(v => config.baseURL + v);
 
     const token = store.state.token;
@@ -36,17 +37,25 @@ axiosInstance.interceptors.request.use(async (config) => {
 
 // 响应拦截器
 axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        if (error.response && error.response.status === 401) {
-            // 处理未授权的情况
-            await toast.error("令牌过期或未授权", error.response.data.message);
-            setTimeout(() => {
-                window.location.href = "/";
-            }, 1500);
+    async response => {
+        if (response.data.extra?.["isTokenUpdated"]) {
+            await updateToken()
         }
-        return Promise.reject(error);
+        return response; // 返回响应数据
+    },
+    async error => {
+
+        if (error.response && error.response.status === 401) {
+            // 处理未授权的情况，例如重定向到登录页面
+            await toast.error("令牌过期或未授权", error.response.data.message);
+            setTimeout(
+                () => window.location.href = "/",
+                1500
+            )
+        }
+        return Promise.reject(error); // 处理其他错误
     }
 );
 
+// 导出 Axios 实例
 export default axiosInstance;
