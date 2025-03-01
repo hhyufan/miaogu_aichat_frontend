044468bd04dc93985e97fcd8b01b90d0eddaf74a
hhyufan <1838248655@qq.com>
2025-03-01 20:41:27 +0800

    fix: 更改远程baseURL访问的域名

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import store from "@/vuex/store.js";
 
 const LOCALHOST_URL = 'http://localhost:8088';
 const LOCALHOST_API_URL = `${LOCALHOST_URL}/edge-config/api/config`;
+const REMOTE_URL = 'https://miaogu.top/api/proxy'
 const API_URL = '/api/config';
 
 async function fetchConfig(url) {
@@ -16,11 +17,7 @@ async function fetchConfig(url) {
 
 export async function getBaseUrl() {
     if (store.state.baseURL) return store.state.baseURL; // 缓存结果
-    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    const url = isLocalhost ? LOCALHOST_URL : API_URL;
-
-    const data = await fetchConfig(url);
-    const apiUrl = isLocalhost ? LOCALHOST_URL : data.apiUrl;
+    const apiUrl = REMOTE_URL;
 
     await store.dispatch('updateBaseUrl', apiUrl);
     return apiUrl; // 返回更新后的 baseUrl
@@ -30,7 +27,7 @@ export async function getPublicKey() {
     if (store.state.publicKey) return store.state.publicKey; // 缓存结果
 
     const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    const url = isLocalhost ? LOCALHOST_API_URL : API_URL;
+    const url = isLocalhost ? REMOTE_URL : API_URL;
 
     const data = await fetchConfig(url);
     const publicKey = isLocalhost ? data.find(item => item.key === 'VUE_APP_PUBLIC_KEY')?.value : data.publicKey;
