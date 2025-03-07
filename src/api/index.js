ecb3a83b80c157e228df20d881157da3d6e532e2
hhyufan <1838248655@qq.com>
2025-03-07 12:55:10 +0800

    chore: 改用import.meta.env编写环境变量，不再使用edge-config

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,16 @@
 import store from "@/vuex/store.js";
 
-const REMOTE_URL = 'https://yuki.cmyam.net/miaogu_api';
-const REMOTE_API_URL = `${REMOTE_URL}/edge-config/api/config`;
-const API_URL = '/api/config';
+const requireEnv = (variableName) => {
+    const value = import.meta.env[variableName];
+    if (!value) {
+        throw new Error(`${variableName} Not Empty!`);
+    }
+    return value;
+}
+
+const REMOTE_URL = requireEnv('VITE_REMOTE_URL');
+const LOCAL_HOST = requireEnv('VITE_LOCAL_HOST');
+const PUBLIC_KEY = requireEnv('VITE_PUBLIC_KEY');
 
 async function fetchConfig(url) {
     try {
@@ -16,21 +24,16 @@ async function fetchConfig(url) {
 
 export async function getBaseUrl() {
     if (store.state.baseURL) return store.state.baseURL; // 缓存结果
-    await store.dispatch('updateBaseUrl', REMOTE_URL);
-    return REMOTE_URL; // 返回更新后的 baseUrl
+    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    const baseUrl = isLocalhost ? LOCAL_HOST : REMOTE_URL
+    await store.dispatch('updateBaseUrl', baseUrl);
+    return baseUrl; // 返回更新后的 baseUrl
 }
 
 export async function getPublicKey() {
     if (store.state.publicKey) return store.state.publicKey; // 缓存结果
-
-    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    const url = isLocalhost ? REMOTE_API_URL : API_URL;
-
-    const data = await fetchConfig(url);
-    const publicKey = isLocalhost ? data.find(item => item.key === 'VUE_APP_PUBLIC_KEY')?.value : data.publicKey;
-
-    await store.dispatch('updatePublicKey', publicKey);
-    return publicKey; // 返回更新后的 publicKey
+    await store.dispatch('updatePublicKey', PUBLIC_KEY);
+    return PUBLIC_KEY; // 返回更新后的 publicKey
 }
 
 // 在应用启动时初始化 baseUrl
